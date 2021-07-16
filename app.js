75302e42ca89aa4b4215f02f359da4c2c6b5340e
12358huaname <51397223+12358hua@users.noreply.github.com>
2021-07-16 14:56:11 +0800

    增加跨域处理

--- app.js
+++ app.js
@@ -13,7 +13,20 @@ const NoticeRouter=require('./routes/NoticeRouter')
 
 // error handler
 onerror(app)
-app.use(cors());
+// app.use(cors());
+app.use(cors({
+  origin: function(ctx) {
+    if (ctx.url === '/test') {
+      return false;
+    }
+    return '*';
+  },
+  maxAge: 5, //指定本次预检请求的有效期，单位为秒。
+  credentials: true, //是否允许发送Cookie
+  allowMethods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], //设置所允许的HTTP请求方法'
+  allowHeaders: ['Content-Type', 'Authorization', 'Accept'], //设置服务器支持的所有头信息字段
+  exposeHeaders: ['WWW-Authenticate', 'Server-Authorization'] //设置获取其他自定义字段
+}));
 
 // middlewares
 app.use(bodyparser({
